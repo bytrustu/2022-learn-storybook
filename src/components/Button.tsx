b6460c87d74992b9103371f14ff35415d3967314
bytrustu <bytrustu@gmail.com>
2022-02-21 17:38:30 +0900

    [ADD] Storybook main preview 추가

---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled'
+
+const Button = () => {
+  return (
+    <ButtonElement>Button</ButtonElement>
+  )
+}
+
+const ButtonElement = styled.button`
+  height: 46px;
+  padding: 0 24px;
+  font-size: 16px;
+  border-radius: 8px;
+  background-color: antiquewhite;
+`
+
+export default Button
